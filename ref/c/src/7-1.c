b33224695b9aa5180ef869f35b7aa682dbf00db1
zhudin <279388213@qq.com>
2019-09-22 16:54:59 +0800

    exercise update

---
+++ ref/c/src/7-1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int x;
+int y;
+int addtwonum()
+{
+    extern int x;
+    extern int y;
+
+    x = 1;
+    y = 2;
+
+    return x + y;
+}
+
+int main()
+{
+    int result;
+
+    result = addtwonum();
+
+    printf("result = %d \n", result);
+
+    return 0;
+}
---
+++ ref/c/src/7-2.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    int a;
+    int b;
+
+    printf("a's address is %p\n", &a);
+    printf("b's address is %p\n", &b);
+
+    return 0;
+}
