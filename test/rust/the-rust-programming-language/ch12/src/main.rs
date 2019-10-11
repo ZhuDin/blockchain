36e26e9b0e6fd2c9e59e5d952cbbcd3ef0f6fd8a
zhudin <279388213@qq.com>
2019-10-11 16:52:32 +0800

    the rust programming language ch12 ok

---
+++ test/rust/the-rust-programming-language/ch12/src/main.rs
@@ -0,0 +1,22 @@
+#![allow(unused_variables)]
+use std::env;
+use std::process;
+
+use ch12::Config;
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    
+    let config = Config::new(&args).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    
+    if let Err(e) = ch12::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
