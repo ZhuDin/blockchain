1c8b7a5d6c1aea26ced110ebec9f51473c25f210
zhudin <279388213@qq.com>
2019-10-20 08:29:57 +0800

    the rust programming language ch14 finished

---
+++ test/rust/the-rust-programming-language/ch13/src/main.rs
@@ -0,0 +1,20 @@
+#![allow(unused_variables)]
+use std::env;
+use std::process;
+
+use ch13::Config;
+
+fn main() {
+    let config = Config::new(env::args()).unwrap_or_else(|err| {
+        eprintln!("Problem parsing arguments: {}", err);
+        process::exit(1);
+    });
+
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
+    
+    if let Err(e) = ch13::run(config) {
+        eprintln!("Application error: {}", e);
+        process::exit(1);
+    }
+}
---
+++ test/rust/the-rust-programming-language/ch14/adder/src/main.rs
@@ -0,0 +1,6 @@
+use add_one;
+
+fn main() {
+    let num = 0;
+    println!("Hello, world! {} plus one is {}!", num, add_one::add_one(num));
+}
